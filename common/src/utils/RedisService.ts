ce2fd945568498829871e2966d9738b2524c9c26
parthstark <parthstarky@gmail.com>
2024-08-16 14:03:28 +0530

    separate out redisStore and redisQueue clients

--- common/src/utils/RedisService.ts
+++ common/src/utils/RedisService.ts
@@ -13,18 +13,24 @@ import {
 
 class RedisService {
     private static instance: RedisService;
-    private redisClient: RedisClientType;
-    private pubsubClient: RedisClientType;
+    private redisStoreClient: RedisClientType;
+    private redisQueueClient: RedisClientType;
+    private redisPubSubClient: RedisClientType;
 
     private constructor() {
-        this.redisClient = createClient();
-        this.pubsubClient = createClient();
+        this.redisStoreClient = createClient();
+        this.redisQueueClient = createClient();
+        this.redisPubSubClient = createClient();
 
-        this.redisClient.on('error', (err) => {
+        this.redisStoreClient.on('error', (err) => {
             console.error('redis error:', err);
         });
 
-        this.pubsubClient.on('error', (err) => {
+        this.redisQueueClient.on('error', (err) => {
+            console.error('redis error:', err);
+        });
+
+        this.redisPubSubClient.on('error', (err) => {
             console.error('redis pubsub error:', err);
         });
     }
@@ -32,19 +38,20 @@ class RedisService {
     public static async getInstance(): Promise<RedisService> {
         if (!RedisService.instance) {
             const service = new RedisService();
-            await service.redisClient.connect();
-            await service.pubsubClient.connect();
+            await service.redisStoreClient.connect();
+            await service.redisQueueClient.connect();
+            await service.redisPubSubClient.connect();
             RedisService.instance = service;
         }
         return RedisService.instance;
     }
 
     public async pushTransactionPreProcessorQueue(transactionRequest: TransactionRequest): Promise<void> {
-        await this.redisClient.rPush(TRANSACTION_PRE_PROCESSOR_QUEUE, JSON.stringify(transactionRequest));
+        await this.redisQueueClient.rPush(TRANSACTION_PRE_PROCESSOR_QUEUE, JSON.stringify(transactionRequest));
     }
 
     public async popTransactionPreProcessorQueue(): Promise<TransactionRequest> {
-        const { element } = await this.redisClient.blPop(TRANSACTION_PRE_PROCESSOR_QUEUE, 0) ?? {};
+        const { element } = await this.redisQueueClient.blPop(TRANSACTION_PRE_PROCESSOR_QUEUE, 0) ?? {};
         if (!element) {
             return emptyTransaction
         }
@@ -62,7 +69,7 @@ class RedisService {
         const senderBalanceKey = `balance:${senderId}`;
         const recipientBalanceKey = `balance:${recipientId}`;
 
-        const multi = this.redisClient.multi();
+        const multi = this.redisStoreClient.multi();
         multi.set(senderBalanceKey, newSenderBalanceInPaise);
         multi.set(recipientBalanceKey, newRecipientBalanceInPaise);
         await multi.exec();
@@ -74,20 +81,20 @@ class RedisService {
             transactionStatus,
             timestamp,
         })
-        await this.pubsubClient.publish(pubsubChannel, pubsubMessage);
+        await this.redisPubSubClient.publish(pubsubChannel, pubsubMessage);
     }
 
     public subscribeToTransaction(transactionId: string, callback: (message: string) => void): void {
         const pubsubChannel = `transaction:${transactionId}`;
-        this.pubsubClient.subscribe(pubsubChannel, callback);
+        this.redisPubSubClient.subscribe(pubsubChannel, callback);
     }
 
     public async pushTransactionPreDBWriterQueue(transactionDBData: ExecutedTransaction): Promise<void> {
-        await this.redisClient.rPush(TRANSACTION_PRE_DB_WRITER_QUEUE, JSON.stringify(transactionDBData));
+        await this.redisQueueClient.rPush(TRANSACTION_PRE_DB_WRITER_QUEUE, JSON.stringify(transactionDBData));
     }
 
     public async popTransactionPreDBWriterQueue(): Promise<ExecutedTransaction> {
-        const { element } = await this.redisClient.blPop(TRANSACTION_PRE_DB_WRITER_QUEUE, 0) ?? {};
+        const { element } = await this.redisQueueClient.blPop(TRANSACTION_PRE_DB_WRITER_QUEUE, 0) ?? {};
         if (!element) {
             return emptyExecutedTransaction
         }
@@ -98,32 +105,32 @@ class RedisService {
 
     public async getBalance(userId: string): Promise<number> {
         const accountIdKey = `balance:${userId}`;
-        const accountIdBalanceString = await this.redisClient.get(accountIdKey);
+        const accountIdBalanceString = await this.redisStoreClient.get(accountIdKey);
         const accountIdBalanceInPaise = parseInt(accountIdBalanceString || '0', 10);
         return accountIdBalanceInPaise
     }
 
     public async setBalance({ userId, balanceInPaise }: setBalanceRequest): Promise<void> {
         const accountIdKey = `balance:${userId}`;
-        await this.redisClient.set(accountIdKey, balanceInPaise);
+        await this.redisStoreClient.set(accountIdKey, balanceInPaise);
     }
 
     public async signupUser({ username, hashedPassword }: userRequest): Promise<signupUserResponse> {
-        const savedHash = await this.redisClient.get(`user:${username}`);
+        const savedHash = await this.redisStoreClient.get(`user:${username}`);
         if (savedHash) {
             return { alreadyExists: true }
         }
 
-        await this.redisClient.set(`user:${username}`, hashedPassword);
+        await this.redisStoreClient.set(`user:${username}`, hashedPassword);
         return { alreadyExists: false }
     }
 
     public async getUserHashedPassword({ username }: getUserHashedPasswordRequest): Promise<string | null> {
-        return await this.redisClient.get(`user:${username}`);
+        return await this.redisStoreClient.get(`user:${username}`);
     }
 
     public async pushSignupUserQueue(user: userRequest): Promise<void> {
-        await this.redisClient.rPush(SIGNUP_USER_QUEUE, JSON.stringify(user));
+        await this.redisQueueClient.rPush(SIGNUP_USER_QUEUE, JSON.stringify(user));
     }
 }
 
